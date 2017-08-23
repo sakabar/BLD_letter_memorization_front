9e748a1cbcb2a6fa42cf5cb328bc80041cafeba2
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2017-08-23 23:52:07 +0900

    記憶すべき文字は画面には表示せず、取得した音声データを耳で聞いて覚える。

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+// require_once "vendor/autoload.php";
+// require_once "vendor/tetsuwo/voicetext-api/src/Tetsuwo/VoiceText/API/Client.php";
+
 use Illuminate\Http\Request;
+use Tetsuwo\VoiceText\API;
+use josegonzalez\Dotenv\Loader as Dotenv;
 
 class FrontController extends Controller
 {
@@ -22,13 +27,13 @@ class FrontController extends Controller
       $ind_arr[$b] = $tmp;
     }
 
-    $ans = "";
+    $ans = [];
     for($i = 0; $i < $len; $i++)
     {
       $s = $arr[$ind_arr[$i]];
       $str_len = mb_strlen($s, 'utf8');
       $char_ind = rand(0, $str_len - 1);
-      $ans .= mb_substr($s, $char_ind, 1, 'utf8');
+      $ans[] = mb_substr($s, $char_ind, 1, 'utf8');
     }
 
     //最後に、分析のループの終了を表す1文字を追加
@@ -36,7 +41,7 @@ class FrontController extends Controller
     $rand_str = $arr[$ind];
     $str_len = mb_strlen($rand_str, 'utf8');
     $char_ind = rand(0, $str_len - 1);
-    $ans .= mb_substr($rand_str, $char_ind, 1, 'utf8');
+    $ans[] = mb_substr($rand_str, $char_ind, 1, 'utf8');
 
     return $ans;
   }
@@ -46,8 +51,37 @@ class FrontController extends Controller
     $edge_letters = array("あか", "いき", "うく", "えけ", "さし", "すせ", "そん", "たな", "ちに", "つぬ", "てね");
     $corner_letters = array("あいう", "かくこ", "さすそ", "たつと", "なぬの", "はふほ", "まむも");
 
-    $edge_str = $this->gen_rand($edge_letters);
-    $corner_str = $this->gen_rand($corner_letters);
-    return view('index', ['edge_str' => $edge_str, 'corner_str' => $corner_str]);
-  }
+    $edge_str_arr = $this->gen_rand($edge_letters);
+    $corner_str_arr = $this->gen_rand($corner_letters);
+
+    Dotenv::load([
+      'filepath' => __DIR__ . '/../../../.env',
+      'toEnv' => true
+    ]);
+
+    $client = new \Tetsuwo\VoiceText\API\Client($_ENV['VOICE_TEXT_TOKEN'], '');
+
+    $edge_voice_arr = [];
+    for($i = 0; $i < count($edge_str_arr); $i++)
+    {
+      $edge_voice_arr[] = base64_encode($client->getTts(array(
+        'text'    => $edge_str_arr[$i],
+        'speaker' => 'hikari'
+      )));
+    }
+
+    $corner_voice_arr = [];
+    for($i = 0; $i < count($corner_str_arr); $i++)
+    {
+      $corner_voice_arr[] = base64_encode($client->getTts(array(
+        'text'    => $corner_str_arr[$i],
+        'speaker' => 'hikari'
+      )));
+    }
+
+    return view('index', ['edge_str_arr' => $edge_str_arr,
+                          'corner_str_arr' => $corner_str_arr,
+                          'edge_voice_arr' => $edge_voice_arr,
+                          'corner_voice_arr' => $corner_voice_arr]);
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,33 @@
   <head>
     <title>BLD</title>
     <link href="css/style.css" rel="stylesheet" type="text/css">
+    <script language="javascript" type="text/javascript">
+      var corner_voice_arr = [];
+      var edge_voice_arr = [];
+  </script>
   </head>
   <body>
     コーナー
     <br>
-    @for ($i = 0; $i < mb_strlen($corner_str, 'utf8'); $i += 2)
-      {{ mb_substr($corner_str, $i, 2, 'utf8') }}<br>
+    @for ($i = 0; $i < count($corner_str_arr); $i++)
+      <script language="javascript" type="text/javascript">
+        corner_voice_arr.push(new Audio("data:audio/wav;base64, {{ $corner_voice_arr[$i] }}"));
+      </script>
+      {{-- $corner_str_arr[$i] --}}
+      <input type="button" value="音声" onclick="corner_voice_arr[{{$i}}].play();"/> {{ sprintf("%02d", $i+1) }}文字目 <input type="button" value="聞き取れない" onclick="alert('{{ $corner_str_arr[$i] }}');"/>
+      <br>
     @endfor
     <br>
     エッジ
     <br>
-    @for ($i = 0; $i < mb_strlen($edge_str, 'utf8'); $i += 2)
-      {{ mb_substr($edge_str, $i, 2, 'utf8') }}<br>
+    @for ($i = 0; $i < count($edge_str_arr); $i++)
+      <script language="javascript" type="text/javascript">
+        edge_voice_arr.push(new Audio("data:audio/wav;base64, {{ $edge_voice_arr[$i] }}"));
+      </script>
+      {{-- $edge_str_arr[$i] --}}
+      <input type="button" value="音声" onclick="edge_voice_arr[{{$i}}].play();"/> {{ sprintf("%02d", $i+1) }}文字目 <input type="button" value="聞き取れない" onclick="alert('{{ $edge_str_arr[$i] }}');"/>
+      <br>
     @endfor
+
   </body>
 </html>
